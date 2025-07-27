a9d3863c1b0c3c8729657a4faf2e3e81519d4c55
Alex Gomez <alxgomez123@gmail.com>
2025-07-26 20:00:05 -0700

    updated reqs, added vc joining

--- src/music_player.py
+++ src/music_player.py
@@ -12,6 +12,29 @@ class MusicPlayer(commands.Cog):
     async def test(self, ctx: Context) -> None:
         await ctx.send('Testing')
 
+    @commands.command(help='Tells the bot to join the voice channel')
+    async def join(self, ctx):
+        if not ctx.message.author.voice:
+            await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+            return
+        else:
+            channel = ctx.message.author.voice.channel
+        await channel.connect()
+
+    @commands.command(help='To make the bot leave the voice channel')
+    async def leave(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_connected():
+            await voice_client.disconnect()
+        else:
+            await ctx.send("The bot is not connected to a voice channel.")
+
+    @commands.command()
+    async def play(self, ctx):
+        voice_client = ctx.message.guild.voice_client
+        async with ctx.typing():
+            voice_client.play(discord.FFmpegOpusAudio(executable='ffmpeg.exe', source='gun shot.mp3'))
+        await ctx.send('Now Playing')
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(MusicPlayer(bot))
